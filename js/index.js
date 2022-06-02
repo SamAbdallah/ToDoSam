f46cda6c65be508e922b9bca022f886e6e7bc517
SamAbdallah <saa081@student.bau.edu.lb>
2022-06-02 23:43:57 +0300

    completing load function and start with add task function

--- js/index.js
+++ js/index.js
@@ -15,3 +15,38 @@
       // check if localStorage has any tasks
       // if not then return
       if (localStorage.getItem("tasks") == null) return;
+
+      // Get the tasks from localStorage and convert it to an array
+      let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+      console.log(tasks);
+
+      // Loop through the tasks and add them to the list
+      tasks.forEach((task,index) => {
+        const list = document.querySelector("ul");
+        const li = document.createElement("li");
+        li.innerHTML = `
+           <input type="text" value="${index}" class="task">
+          <input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+          <input type="text" value="${task.title}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+          <input type="text" value="${task.description}" class="task" >
+          <input type="text" value="${task.point}" class="task">
+          <input type="text" value="${task.time}" class="task">
+          <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+        list.insertBefore(li, list.children[0]);
+      });
+    }
+
+     function addTask() {
+      let time = new Date().toISOString()
+      let title = document.getElementById("title").value
+      let description = document.getElementById("desc").value
+      let point = document.getElementById("point").value
+      const task = {title:title,description:description,point:point,time:time,completed:false}
+      const list = document.querySelector("ul");
+      // return if task is empty
+      if (task.value === "") {
+        alert("Please add some task!");
+        return false;
+      }
+
+      
